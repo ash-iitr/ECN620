b3dc129c2457fc2efec0eb05db3416b47be3676a
Ashwani Kumar <157246274+ash-iitr@users.noreply.github.com>
2024-04-05 14:16:47 +0530

    Update CellFree.m
    Working for statistical atleast

--- CellFree.m
+++ CellFree.m
@@ -2,25 +2,31 @@ clc;
 close all;
 clear;
 
-K = 20; % Number of users
+Kusers = [10 20]; % Number of users
 Map = 20:20:200; % Number of APs
 rho_p_cf = 1;
-tau_cf = K;
+%tau_cf = K;
 %Beta = ones(M,K); %largescale path loss
 rho_d_cf = 1; % Allocating all power to downlink signals
-
-Rd_k = zeros(K,length(Map));
+iters =1; % for statistical achievable rate
+for ku = 1:length(Kusers)
+    K = Kusers(ku);
+    tau_cf = K;
+for it = 1:iters %for statistical achievable rate
+Rs_k = zeros(K,length(Map));
 for mm = 1:length(Map)
 M = Map(mm);
 Beta = ones(M,K); %largescale path loss
 %% Pilot generation
 
-% Generate a random Hermitian matrix
-A = randn(K) + 1i * randn(K); % Complex random matrix
-A = (A + conj(A')) / 2; % Ensuring Hermitian property
+% % Generate a random Hermitian matrix
+% A = randn(K) + 1i * randn(K); % Complex random matrix
+% A = (A + conj(A')) / 2; % Ensuring Hermitian property
+% 
+% % Compute eigenvectors and eigenvalues
+% [pilots, ~] = eig(A);
 
-% Compute eigenvectors and eigenvalues
-[pilots, ~] = eig(A);
+pilots = sqrt(1/2)*(eye(K) + 1i * eye(K));
 
 check = norm(pilots(:,5))^2
 check2 = abs(pilots(:,1)'*pilots(:,2))^2  % tends to 0 as n increases
@@ -73,7 +79,7 @@ for m = 1:M
 end
 
 
-%% Equation 24 begins 
+%% Equation 24 - pilots and C needed 
 for kth = 1:K
     num_s = 0;
     for msn = 1:M
@@ -99,9 +105,112 @@ for kth = 1:K
     end
 
 
-    Rd_k(kth,mm) = log2(1+ (rho_d_cf*(num_s^2))/(rho_d_cf*den_s1 + rho_d_cf*den_s2 +1));
+    Rs_k(kth,mm) = log2(1+ (rho_d_cf*(num_s^2))/(rho_d_cf*den_s1 + rho_d_cf*den_s2 +1));
+end
+    % %% Equation 26 begins
+    % num_e = 0;
+    % for men = 1:M
+    %     num_e = num_e + sqrt(Eta(men,kth))*G(men,kth)*Gcap(men,kth);
+    % end
+    % 
+    % den_e1 = 0;
+    % for ked = 1:K
+    %     if(ked~=kth)
+    %         den_e_i = 0;
+    %         for med = 1:M
+    %             den_e_i = den_e_i + sqrt(Eta(men,ked))*G(men,kth)*Gcap(men,ked);
+    %         end
+    %         den_e1 = den_e1 + (abs(den_e_i))^2;
+    %     end
+    % end
+    % 
+    % Re_k(kth,mm) = log2(1+ ((rho_d_cf*abs(num_e)^2)/(rho_d_cf*den_e1 + 1)));
+    itee = 100;
+    for ite = 1:itee % for equation 26
+        %% Generate channel coefficients MxK
+        G = sqrt(1/2) * (randn(M,K) + 1i * randn(M,K)); % Complex random matrix
 
-end % end of loop for kth user
+        % large scale pathloss Beta = 1 in each case.
+
+        Wp = sqrt(1/2) * (randn(K,M) + 1i * randn(K,M)); %Noise while receiving pilot vectors at AP
+
+        Yp = zeros(K,M); % Equation 2
+        for m = 1:M
+            temp = zeros(K,1);
+            for k=1:K
+                temp = temp + G(m,k)*pilots(:,k);
+            end
+            Yp(:,m) = sqrt(rho_p_cf*tau_cf)*temp + Wp(:,m);
+        end
+
+        Yhatp = zeros(M,K); % Equation 3
+        for m = 1:M
+            for k=1:K
+                Yhatp(m,k) = pilots(:,k)'*Yp(:,m);
+            end
+        end
+
+        C = zeros(M,K);
+        for m = 1:M
+            for k=1:K
+                temp2 = 0;
+                for kk = 1:K
+                    temp2 = temp2 + Beta(m,kk)*(abs(pilots(:,k)'*pilots(:,kk)))^2;
+                end
+                C(m,k) = (sqrt(rho_p_cf*tau_cf)*Beta(m,k))/(tau_cf*rho_p_cf*temp2 + 1);
+            end
+        end
+
+        Gcap = C.*Yhatp; % Equation 4
+        Gamma = sqrt(rho_p_cf*tau_cf)* Beta.*C;
+        Eta = zeros(M,K);
+        for m = 1:M
+            for k=1:K
+                Eta(m,k) = 1/(K*Gamma(m,k));
+            end
+        end
+             %% Equation 26 begins
+             for kte=1:K
+                 num_e = 0;
+                 for men = 1:M
+                     num_e = num_e + sqrt(Eta(men,kte))*G(men,kte)*Gcap(men,kte);
+                 end
+
+                 den_e1 = 0;
+                 for ked = 1:K
+                     if(ked~=kte)
+                         den_e_i = 0;
+                         for med = 1:M
+                             den_e_i = den_e_i + sqrt(Eta(men,ked))*G(men,kte)*Gcap(men,ked);
+                         end
+                         den_e1 = den_e1 + (abs(den_e_i))^2;
+                     end
+                 end
+
+                 Re_k(kte,ite) = log2(1+ ((rho_d_cf*((abs(num_e))^2))/(rho_d_cf*den_e1 + 1)));
+
+             end
+
+    end
+TempExactCap = sum(Re_k,2)/itee;
+TempExactCap2(mm,ku) = sum(TempExactCap,1)/K;
+
+%end % end of loop for kth user
 end %end of loop for M antennas at AP
-Cap = sum(Rd_k,1)/K;
-plot(Map,Cap);
+Cap(it,:) = sum(Rs_k,1)/K;
+%Capexact(it,:) = sum(Re_k,1)/K;
+end
+Cap2(ku,:) = sum(Cap,1)/iters; % Using statistical
+%Capexact2(ku,:) = sum(Capexact,1)/iters; % Using exact realization
+end
+
+plot(Map,Cap2(1,:),'-.');
+hold on;
+plot(Map,Cap2(2,:),'-.');
+hold on;
+% plot(Map,Capexact2(1,:));
+% hold on;
+% plot(Map,Capexact2(2,:));
+legend('Statistical - K=10','Statistical - K=20', 'Exact - K=10', 'Exact - K=20');
+xlabel('Number of APs');
+ylabel('Achievable rate per user');
