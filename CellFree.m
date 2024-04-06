f5d54d5a147b4be718bcb20bc69396d45c129833
Ashwani Kumar <157246274+ash-iitr@users.noreply.github.com>
2024-04-07 02:40:38 +0530

    Update CellFree.m

--- CellFree.m
+++ CellFree.m
@@ -125,7 +125,7 @@ end
     % end
     % 
     % Re_k(kth,mm) = log2(1+ ((rho_d_cf*abs(num_e)^2)/(rho_d_cf*den_e1 + 1)));
-    itee = 100;
+    itee = 10;
     for ite = 1:itee % for equation 26
         %% Generate channel coefficients MxK
         G = sqrt(1/2) * (randn(M,K) + 1i * randn(M,K)); % Complex random matrix
@@ -135,45 +135,55 @@ end
         Wp = sqrt(1/2) * (randn(K,M) + 1i * randn(K,M)); %Noise while receiving pilot vectors at AP
 
         Yp = zeros(K,M); % Equation 2
-        for m = 1:M
+        for m1 = 1:M
             temp = zeros(K,1);
-            for k=1:K
-                temp = temp + G(m,k)*pilots(:,k);
+            for k1=1:K
+                temp = temp + G(m1,k1)*pilots(:,k1);
             end
-            Yp(:,m) = sqrt(rho_p_cf*tau_cf)*temp + Wp(:,m);
+            Yp(:,m1) = sqrt(rho_p_cf*tau_cf)*temp + Wp(:,m1);
         end
 
         Yhatp = zeros(M,K); % Equation 3
-        for m = 1:M
-            for k=1:K
-                Yhatp(m,k) = pilots(:,k)'*Yp(:,m);
+        for m2 = 1:M
+            for k2=1:K
+                Yhatp(m2,k2) = pilots(:,k2)'*Yp(:,m2);
             end
         end
 
         C = zeros(M,K);
-        for m = 1:M
-            for k=1:K
+        for m3 = 1:M
+            for k3=1:K
+
                 temp2 = 0;
                 for kk = 1:K
-                    temp2 = temp2 + Beta(m,kk)*(abs(pilots(:,k)'*pilots(:,kk)))^2;
+                    temp2 = temp2 + (abs(pilots(:,k3)'*pilots(:,kk)))^2;
                 end
-                C(m,k) = (sqrt(rho_p_cf*tau_cf)*Beta(m,k))/(tau_cf*rho_p_cf*temp2 + 1);
+                C(m3,k3) = (sqrt(rho_p_cf*tau_cf))/(tau_cf*rho_p_cf*temp2 + 1);
+
             end
         end
 
-        Gcap = C.*Yhatp; % Equation 4
-        Gamma = sqrt(rho_p_cf*tau_cf)* Beta.*C;
+        %Gcap = C.*Yhatp; % Equation 4
+        Gcap = zeros(M,K);
+        for m4 = 1:M
+            for k4=1:K
+                Gcap(m4,k4) = C(m4,k4)*Yhatp(m4,k4);
+            end
+        end
+
+
+        Gamma = sqrt(rho_p_cf*tau_cf)*C;
         Eta = zeros(M,K);
-        for m = 1:M
-            for k=1:K
-                Eta(m,k) = 1/(K*Gamma(m,k));
+        for m5 = 1:M
+            for k5=1:K
+                Eta(m5,k5) = 1/(K*Gamma(m5,k5));
             end
         end
              %% Equation 26 begins
-             for kte=1:K
+             for kte=1:K % Calculate for each user
                  num_e = 0;
                  for men = 1:M
-                     num_e = num_e + sqrt(Eta(men,kte))*G(men,kte)*Gcap(men,kte);
+                     num_e = num_e + sqrt(Eta(men,kte))*G(men,kte)*conj(Gcap(men,kte));
                  end
 
                  den_e1 = 0;
@@ -181,15 +191,16 @@ end
                      if(ked~=kte)
                          den_e_i = 0;
                          for med = 1:M
-                             den_e_i = den_e_i + sqrt(Eta(men,ked))*G(men,kte)*Gcap(men,ked);
+                             den_e_i = den_e_i + sqrt(Eta(men,ked))*G(men,kte)*conj(Gcap(men,ked));
                          end
                          den_e1 = den_e1 + (abs(den_e_i))^2;
                      end
                  end
 
-                 Re_k(kte,ite) = log2(1+ ((rho_d_cf*((abs(num_e))^2))/(rho_d_cf*den_e1 + 1)));
+                 Re_k(kte) = log2(1+ ((rho_d_cf*((abs(num_e))^2))/(rho_d_cf*den_e1 + 1)));
 
              end
+
 
     end
 TempExactCap = sum(Re_k,2)/itee;
@@ -214,3 +225,59 @@ hold on;
 legend('Statistical - K=10','Statistical - K=20', 'Exact - K=10', 'Exact - K=20');
 xlabel('Number of APs');
 ylabel('Achievable rate per user');
+
+
+
+function [G,Yp,Yhatp,C,Gcap,Gamma,Eta] = generateChannels(M,K)
+G = sqrt(1/2) * (randn(M,K) + 1i * randn(M,K)); % Complex random matrix
+
+        % large scale pathloss Beta = 1 in each case.
+
+        Wp = sqrt(1/2) * (randn(K,M) + 1i * randn(K,M)); %Noise while receiving pilot vectors at AP
+
+        Yp = zeros(K,M); % Equation 2
+        for m1 = 1:M
+            temp = zeros(K,1);
+            for k1=1:K
+                temp = temp + G(m1,k1)*pilots(:,k1);
+            end
+            Yp(:,m1) = sqrt(rho_p_cf*tau_cf)*temp + Wp(:,m1);
+        end
+
+        Yhatp = zeros(M,K); % Equation 3
+        for m2 = 1:M
+            for k2=1:K
+                Yhatp(m2,k2) = pilots(:,k2)'*Yp(:,m2);
+            end
+        end
+
+        C = zeros(M,K);
+        for m3 = 1:M
+            for k3=1:K
+
+                temp2 = 0;
+                for kk = 1:K
+                    temp2 = temp2 + (abs(pilots(:,k3)'*pilots(:,kk)))^2;
+                end
+                C(m3,k3) = (sqrt(rho_p_cf*tau_cf))/(tau_cf*rho_p_cf*temp2 + 1);
+
+            end
+        end
+
+        %Gcap = C.*Yhatp; % Equation 4
+        Gcap = zeros(M,K);
+        for m4 = 1:M
+            for k4=1:K
+                Gcap(m4,k4) = C(m4,k4)*Yhatp(m4,k4);
+            end
+        end
+
+
+        Gamma = sqrt(rho_p_cf*tau_cf)*C;
+        Eta = zeros(M,K);
+        for m5 = 1:M
+            for k5=1:K
+                Eta(m5,k5) = 1/(K*Gamma(m5,k5));
+            end
+        end
+end
